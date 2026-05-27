3b5ca898f41d6ba71e00350737c3af4f8447177a
Doeun <qwaszx080402@naver.com>
2026-05-27 10:05:55 +0900

    fix :: //수정

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export { COOKIE_NAME, ONE_YEAR_MS } from "@/shared/const";
 
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
+export const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL as string)?.replace(/\/$/, "");
 
 export const getLoginUrl = () =>
   `${BACKEND_URL}/oauth2/authorization/kakao`;
